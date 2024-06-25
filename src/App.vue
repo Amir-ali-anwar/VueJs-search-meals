bb1cd89cf467d0e0dcf18f37c94f6f1988b0d6fa
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-06-25 22:49:58 +0500

    added the vye router

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,5 @@
-<script setup>
-</script>
-
 <template>
-  <div class="bg-blue">
-    <h1 class="ext-3xl font-bold underline">erer</h1>
-    <a href="https://vitejs.dev" target="_blank">4
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-
+  <h1 class="text-3xl font-bold underline bg-blue-500">
+    Hello world!
+  </h1>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
